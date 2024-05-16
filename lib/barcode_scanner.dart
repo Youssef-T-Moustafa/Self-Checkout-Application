749d2faae20b747954cb57535263a84b133c5834
Loai-AL-Sabahi <engineerloai108@gmail.com>
2024-05-16 09:31:04 +0800

    back to home button

--- lib/barcode_scanner.dart
+++ lib/barcode_scanner.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:map_project/homepage.dart';
 
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
 class BarcodeScanner extends StatefulWidget {
   @override
   _BarcodeScanner createState() => _BarcodeScanner();
@@ -20,7 +22,12 @@ class _BarcodeScanner extends State<BarcodeScanner> {
       ScanMode.BARCODE,
     );
 
-    DocumentSnapshot documentSnapshot = await _firestore.collection('Swift').doc("Products").collection("ProductDetails").doc(barcodeScanResult).get();
+    DocumentSnapshot documentSnapshot = await _firestore
+        .collection('Swift')
+        .doc("Products")
+        .collection("ProductDetails")
+        .doc(barcodeScanResult)
+        .get();
 
     if (documentSnapshot.exists) {
       setState(() {
@@ -43,20 +50,31 @@ class _BarcodeScanner extends State<BarcodeScanner> {
       appBar: AppBar(
         title: Text('Barcode Scanner'),
       ),
-      body: Center(
-        //After UI is ready fetch data field by field and print
-        child: documentData != {}
-          ?(documentData.isNotEmpty
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('Name: ${documentData['Name']}'),
-                  Text('Description: ${documentData['Description']}'),
-                  Text('Price: ${documentData['Price']}'),
-                ],
-              )
-            : Text('Item was not found'))
-          :Text(""),
+      body: Column(
+        children: [
+          Center(
+            //After UI is ready fetch data field by field and print
+            child: documentData != {}
+                ? (documentData.isNotEmpty
+                    ? Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text('Name: ${documentData['Name']}'),
+                          Text('Description: ${documentData['Description']}'),
+                          Text('Price: ${documentData['Price']}'),
+                        ],
+                      )
+                    : Text('Item was not found'))
+                : Text(""),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (context) => HomePage()));
+            },
+            child: Text('Go Back'),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: scanBarcode,
